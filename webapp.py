84b266bfc0479ef764f33e74691bec4fc5b2b84c
ConnellyR <42652898+ConnellyR@users.noreply.github.com>
2018-11-27 10:26:50 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -9,6 +9,15 @@ def render_main():
 @app.route("/rank")
 def render_page1():
     return render_template('rank.html')
-    
+   
+def get_rank_options(counties):
+    myList=[]
+    for county in counties:
+        if not county["State"] in myList:
+            myList.append(county["State"] )
+    options=""
+    for State in myList:
+        options += Markup("<option value=\"" + State + "\">" + State + "</option>")
+    return options
 if __name__=="__main__":
     app.run(debug=False, port=54321)
